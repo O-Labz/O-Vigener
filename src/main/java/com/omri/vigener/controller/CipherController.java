d03ffcc6141e6fdaa234a0866949c04bfadb94fc
Omri Hopson <hopsonok@jncb.com>
2019-09-27 11:35:19 -0500

    implemented controller

--- src/main/java/com/omri/vigener/controller/CipherController.java
+++ src/main/java/com/omri/vigener/controller/CipherController.java
@@ -1,5 +1,37 @@
 package com.omri.vigener.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.omri.vigener.model.CipherResponse;
+
+@RestController
+@RequestMapping("/api/cipher")
 public class CipherController {
+	
+	@GetMapping("/encrypt")
+	public CipherResponse encrypt () {
+		
+		CipherResponse resp = new CipherResponse ();
+		
+		resp.setSuccess(true);
+		resp.setText("Omri String Encrypted");
+		
+		return resp;
+		
+	}
+	
+	@GetMapping("/decrypt")
+	public CipherResponse decrypt () {
+		
+		CipherResponse resp = new CipherResponse ();
+		
+		resp.setSuccess(true);
+		resp.setText("Omri String Decrypted");
+		
+		return resp;
+		
+	}
 
 }
--- src/main/java/com/omri/vigener/model/CipherResponse.java
+++ src/main/java/com/omri/vigener/model/CipherResponse.java
@@ -1,5 +1,37 @@
 package com.omri.vigener.model;
 
 public class CipherResponse {
+	
+	private String text;
+	
+	private Boolean success;
+
+	/**
+	 * @return the text
+	 */
+	public String getText() {
+		return text;
+	}
+
+	/**
+	 * @param text the text to set
+	 */
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	/**
+	 * @return the success
+	 */
+	public Boolean getSuccess() {
+		return success;
+	}
+
+	/**
+	 * @param success the success to set
+	 */
+	public void setSuccess(Boolean success) {
+		this.success = success;
+	}
 
 }
