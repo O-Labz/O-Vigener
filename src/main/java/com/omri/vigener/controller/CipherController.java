e0050a9eb028affe489a6ed4366a04f157f2b221
Omri Hopson <hopsonok@jncb.com>
2019-09-27 12:00:41 -0500

    updated service and controller with proper structer

--- src/main/java/com/omri/vigener/controller/CipherController.java
+++ src/main/java/com/omri/vigener/controller/CipherController.java
@@ -1,37 +1,57 @@
 package com.omri.vigener.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.omri.vigener.model.CipherRequest;
 import com.omri.vigener.model.CipherResponse;
+import com.omri.vigener.service.CipherService;
 
 @RestController
 @RequestMapping("/api/cipher")
 public class CipherController {
-	
+
+	@Autowired
+	private CipherService cipherservice;
+
+	private CipherResponse response;
+
 	@GetMapping("/encrypt")
-	public CipherResponse encrypt () {
+	public CipherResponse encrypt (@RequestBody CipherRequest cipherRequest) {
 		
-		CipherResponse resp = new CipherResponse ();
-		
-		resp.setSuccess(true);
-		resp.setText("Omri String Encrypted");
-		
-		return resp;
-		
+		response = new CipherResponse ();
+
+		try {
+			//  Block of code to try
+			response = cipherservice.encryptText(cipherRequest);
+		}
+		catch(Exception e) {
+			//  Block of code to handle errors
+			e.getMessage();
+		}
+
+		return response;
 	}
-	
+
 	@GetMapping("/decrypt")
-	public CipherResponse decrypt () {
-		
-		CipherResponse resp = new CipherResponse ();
-		
-		resp.setSuccess(true);
-		resp.setText("Omri String Decrypted");
-		
-		return resp;
-		
+	public CipherResponse decrypt (@RequestBody CipherRequest cipherRequest) {
+
+		response = new CipherResponse ();
+
+		try {
+			//  Block of code to try
+			response = cipherservice.decryptText(cipherRequest);
+		}
+		catch(Exception e) {
+			//  Block of code to handle errors
+			e.getMessage();
+		}
+
+		return response;
+
 	}
 
 }
--- src/main/java/com/omri/vigener/service/CipherService.java
+++ src/main/java/com/omri/vigener/service/CipherService.java
@@ -1,5 +1,34 @@
 package com.omri.vigener.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.omri.vigener.model.CipherRequest;
+import com.omri.vigener.model.CipherResponse;
+
+@Service
 public class CipherService {
+
+	public CipherResponse encryptText(@RequestBody CipherRequest cipherRequest) {
+
+		CipherResponse response = new CipherResponse ();
+
+		response.setSuccess(cipherRequest.getEncrypt());
+		response.setText(cipherRequest.getRawtext());
+
+		return response;
+
+	}
+	
+	public CipherResponse decryptText(@RequestBody CipherRequest cipherRequest) {
+
+		CipherResponse response = new CipherResponse ();
+
+		response.setSuccess(cipherRequest.getEncrypt());
+		response.setText(cipherRequest.getRawtext());
+
+		return response;
+
+	}
 
 }
