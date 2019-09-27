4e9c5edb543da6f0ed3560d3f6bdcf661c394820
Omri Hopson <hopsonok@jncb.com>
2019-09-27 16:50:19 -0500

    implemented cipher algorithms

--- src/main/java/com/omri/vigener/service/CipherService.java
+++ src/main/java/com/omri/vigener/service/CipherService.java
@@ -12,9 +12,22 @@ public class CipherService {
 	public CipherResponse encryptText(@RequestBody CipherRequest cipherRequest) {
 
 		CipherResponse response = new CipherResponse ();
+		
+        String result = "";
+        String plainText = "";
+        
+        plainText = cipherRequest.getRawtext().toLowerCase();
+        for (int i = 0, j = 0; i < plainText.length(); i++)
+        {
+            char c = plainText.charAt(i);
+            if (c < 'a' || c > 'z')
+                continue;
+            result += (char) ((c + cipherRequest.getKey().charAt(j) - 2 * 'a') % 26 + 'a');
+            j = ++j % cipherRequest.getKey().length();
+        }
 
 		response.setSuccess(cipherRequest.getEncrypt());
-		response.setText(cipherRequest.getRawtext());
+		response.setText(result);
 
 		return response;
 
@@ -23,9 +36,22 @@ public class CipherService {
 	public CipherResponse decryptText(@RequestBody CipherRequest cipherRequest) {
 
 		CipherResponse response = new CipherResponse ();
+		
+        String result = "";
+        String encryptedText = "";
+        
+        encryptedText = cipherRequest.getRawtext().toLowerCase();
+        for (int i = 0, j = 0; i < encryptedText.length(); i++)
+        {
+            char c = encryptedText.charAt(i);
+            if (c < 'a' || c > 'z')
+                continue;
+            result += (char) ((c - cipherRequest.getKey().charAt(j) + 26) % 26 + 'a');
+            j = ++j % cipherRequest.getKey().length();
+        }
 
 		response.setSuccess(cipherRequest.getEncrypt());
-		response.setText(cipherRequest.getRawtext());
+		response.setText(result);
 
 		return response;
 
